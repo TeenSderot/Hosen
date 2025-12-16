032e5054635babd846a4c564f070b165590139cb
RazBenLulu <Razbenlulu@gmail.com>
2025-12-16 15:07:38 +0200

    adding add and delete feeling

--- server/routes/feelingsRouter.js
+++ server/routes/feelingsRouter.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
+import { getDb } from "../db.js";
+import { ObjectId } from "mongodb";
 
+const FEELINGS_COLLECTION = "feelings";
 const router = Router();
+const db = await getDb();
+const feelings = db.collection(FEELINGS_COLLECTION);
+
 
 router.get("/", (req, res) => {
     res.status(200).json(feelings);
 });
 
-router.post("/addfeeling", (req, res) => {
+
+
+
+router.post("/addfeeling", async (req, res) => {
     const { title, text } = req.body;
 
     if (!title) {
@@ -18,9 +27,36 @@ router.post("/addfeeling", (req, res) => {
         text: text ?? "",
         createdAt: new Date().toISOString()
     };
-    
-    res.status(200).json({message:"Success"});
+
+    await feelings.insertOne({...newItem});
+
+    res.status(200).json({ message: "Success" });
 
 });
+
+
+
+
+router.delete("/deletefeeling/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await feelings.deleteOne({
+      _id: new ObjectId(id)
+    });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ error: "Feeling not found" });
+    }
+
+    res.status(200).json({ message: "Deleted successfully" });
+
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ error: "Invalid id" });
+  }
+});
+
+
 
 export default router;
